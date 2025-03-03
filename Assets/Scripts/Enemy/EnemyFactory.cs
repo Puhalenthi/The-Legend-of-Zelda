4d1784a76f8a6aad061ba1c9aeecd0605dce5362
Joshua Peisach <itzswirlz2020@outlook.com>
2025-03-03 10:51:51 -0500

    enemy factory: set a predetermined number of enemies to spawn

--- Assets/Scripts/Enemy/EnemyFactory.cs
+++ Assets/Scripts/Enemy/EnemyFactory.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class EnemyFactory : MonoBehaviour
 {
-    public float cooldown = 5.0f;
     public GameObject enemyPrefab;
+    public float cooldown = 5.0f;
+    public int enemiesToSpawn = 4;
     public float xRange;
     public float yRange;
     public float zRange;
@@ -21,13 +22,17 @@ public class EnemyFactory : MonoBehaviour
 
     public IEnumerator SpawnEnemy()
     {
-        while (true)
+        int enemiesSpawned = 0;
+
+        while (enemiesSpawned < enemiesToSpawn)
         {
             float x = Random.Range(-xRange, xRange);
             float y = Random.Range(-yRange, yRange);
             float z = Random.Range(-zRange, zRange);
 
             Instantiate(enemyPrefab, new Vector3(x, y, z), Quaternion.identity);
+            enemiesSpawned++;
+
             yield return new WaitForSeconds(cooldown);
         }
     }
