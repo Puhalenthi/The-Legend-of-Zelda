c16da6246660349b740b08dd4f7f7f1c4d63a20b
Puhalenthi Ramesh Vidhya <57294477+Puhalenthi@users.noreply.github.com>
2025-03-09 23:55:30 -0400

    finish up EnemyFactory

--- Assets/Scripts/Enemy/EnemyFactory.cs
+++ Assets/Scripts/Enemy/EnemyFactory.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class EnemyFactory : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    public float cooldown = 20.0f;
-    public int mobCap = 20;
+    public float cooldown = 5.0f;
+    public int enemiesToSpawn = 48;
     public float xRange;
     public float yRange;
     public float zRange;
@@ -24,8 +24,13 @@ public class EnemyFactory : MonoBehaviour
     {
         int enemiesSpawned = 0;
 
-        while (enemiesSpawned < mobCap)
+        while (enemiesSpawned < enemiesToSpawn)
         {
+            float x = Random.Range(-xRange, xRange);
+            float y = Random.Range(-yRange, yRange);
+            float z = Random.Range(-zRange, zRange);
+
+            //Instantiate(enemyPrefab, new Vector3(x, y, z), Quaternion.identity);
             Instantiate(enemyPrefab, new Vector3(-7.0f, 10.0f, 0.0f), Quaternion.identity);
             Instantiate(enemyPrefab, new Vector3(-14.0f, 4.5f, 0.0f), Quaternion.identity);
             Instantiate(enemyPrefab, new Vector3(-26.0f, 7.5f, 0.0f), Quaternion.identity);
@@ -33,10 +38,12 @@ public class EnemyFactory : MonoBehaviour
             Instantiate(enemyPrefab, new Vector3(-18.0f, 18.0f, 0.0f), Quaternion.identity);
             Instantiate(enemyPrefab, new Vector3(-2.0f, 20.0f, 0.0f), Quaternion.identity);
             Instantiate(enemyPrefab, new Vector3(-2.0f, 29.0f, 0.0f), Quaternion.identity);
-            Instantiate(enemyPrefab, new Vector3(-20.0f, 17.0f, 0.0f), Quaternion.identity);
             Instantiate(enemyPrefab, new Vector3(22.0f, 30.0f, 0.0f), Quaternion.identity);
             Instantiate(enemyPrefab, new Vector3(42.0f, 29.5f, 0.0f), Quaternion.identity);
-            enemiesSpawned++;
+            Instantiate(enemyPrefab, new Vector3(37.8f, -0.26f, 0.0f), Quaternion.identity);
+            Instantiate(enemyPrefab, new Vector3(39.0f, 9.83f, 0.0f), Quaternion.identity);
+            Instantiate(enemyPrefab, new Vector3(9.55f, 4.77f, 0.0f), Quaternion.identity);
+            enemiesSpawned += 12;
 
             yield return new WaitForSeconds(cooldown);
         }
